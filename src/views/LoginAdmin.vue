2cabe634d3e2cc81f84b81b052a9e51d5ffa505f
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2024-11-15 12:55:19 -0300

    Implement alert

--- src/views/LoginAdmin.vue
+++ src/views/LoginAdmin.vue
@@ -1,6 +1,13 @@
 <template>
-    <form class="formulario max-w-sm mx-auto" @submit.prevent="fazerLogin">
+    <form class="formulario max-w-sm mx-auto mb-5" @submit.prevent="fazerLogin">
         <h1 class="text-2xl my-3.5 text-center uppercase font-bold">Login de administrador</h1>
+
+        <!-- Alerta de erro -->
+        <div v-if="erroMensagem" class="mt-5 mb-5 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" role="alert">
+            <p class="font-bold">Erro</p>
+            <p>{{ erroMensagem }}</p>
+        </div>
+
         <div class="mb-5">
             <label for="email" class="block mb-2 text-sm font-medium text-cyan-950 dark:text-cyan-950">E-mail</label>
             <input v-model="email" type="email" id="email" required
@@ -23,35 +30,43 @@
 import axios from 'axios';
 import { Options, Vue } from 'vue-class-component';
 
-@Options({})
+@Options({
+    components: {}
+})
 export default class LoginAdmin extends Vue {
-    email = '';
-    senha = '';
+    email = ''
+    senha = ''
+    erroMensagem: string | null = null  // Variável para armazenar a mensagem de erro
 
     async fazerLogin() {
         try {
             const response = await axios.post('http://localhost/Projetos/ja-quero-pijamas/backend/api/login.php', {
                 email: this.email,
                 senha: this.senha
-            });
+            })
 
-            console.log('Resposta do servidor:', response.data);
+            console.log('Resposta do servidor:', response.data)
 
             if (response.data.success) {
                 // Salvar a autenticação no localStorage
-                localStorage.setItem('user-authenticated', 'true');
-
-                alert('Login realizado com sucesso!');
-                this.$router.push('/adicionar-produto');
+                localStorage.setItem('user-authenticated', 'true')
+                this.$router.push('/adicionar-produto')
             } else {
-                alert(response.data.message || 'Erro no login!');
+                this.erroMensagem = response.data.message || 'Erro no login!'
+                this.limparErroDepoisDeTempo()
             }
         } catch (error) {
-            console.error('Erro na requisição:', error);
-            alert('Erro ao tentar fazer login. Verifique suas credenciais.');
+            console.error('Erro na requisição:', error)
+            this.erroMensagem = 'Erro ao tentar fazer login. Verifique suas credenciais.'
+            this.limparErroDepoisDeTempo()
         }
     }
 
+    limparErroDepoisDeTempo() {
+        setTimeout(() => {
+            this.erroMensagem = null
+        }, 5000)
+    }
 }
 </script>
 
