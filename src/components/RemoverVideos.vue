926bc2b116231c51cd7ddb2ec4619a9d6f91d458
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2024-11-15 22:16:17 -0300

    Implement adicionarVideo

---
+++ src/components/RemoverVideos.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container mx-auto p-4">
+        <h1 class="text-2xl font-bold text-center mb-6">Vídeos de alguns de nossos produtos</h1>
+        <div class="flex flex-col md:flex-row md:space-x-6 space-y-4 md:space-y-0 justify-center items-center">
+            <div class="relative w-full md:w-1/4">
+                <div class="absolute top-2 right-2 z-10">
+                    <botao-remover-video />
+                </div>
+                <video controls class="w-full h-auto rounded-lg">
+                    <source src="../assets/imgs/video1.mp4" type="video/mp4">
+                    Seu navegador não suporta o elemento de vídeo.
+                </video>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import Produtos from '../components/Produtos.vue'
+import QuemSomos from '../components/QuemSomos.vue'
+import Videos from '../components/Videos.vue'
+import BotaoRemoverVideo from '../components/BotaoRemoverVideo.vue'
+
+@Options({
+    components: {
+        Produtos,
+        QuemSomos,
+        Videos,
+        BotaoRemoverVideo
+    }
+})
+export default class RemoverVideo extends Vue { }
+</script>
--- src/views/AdicionarProduto.vue
+++ src/views/AdicionarProduto.vue
@@ -5,7 +5,7 @@
             <i class="fa-solid fa-right-from-bracket"></i> Logout</button>
     </div>
 
-    <form class="formulario max-w-sm mx-auto" @submit.prevent="adicionarProduto">
+    <div class="flex justify-center">
         <div v-if="mensagem"
             :class="['mt-5 mb-5 border-l-4 p-4', tipoMensagem === 'success' ? 'bg-green-100 border-green-500 text-green-700' : 'bg-red-100 border-red-500 text-red-700']"
             role="alert">
@@ -13,20 +13,51 @@
             <p class="font-bold" v-if="tipoMensagem === 'success'">Sucesso</p>
             <p>{{ mensagem }}</p>
         </div>
-        <h1 class="text-2xl my-3.5 text-center uppercase font-bold">Adicionar produto</h1>
-        <div class="mb-5">
-            <label class="block mb-2 text-sm font-medium text-gray-900" for="default_size">Escolha a imagem do
-                produto</label>
-            <input
-                class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
-                id="default_size" ref="fileInput" type="file" @change="processarImagem">
-        </div>
-        <button type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-            Adicionar Produto</button>
-    </form>
+    </div>
 
-    <div class="bg-gray-200 mt-20">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10 px-5">
+        <!--Card de adicionar Produto-->
+        <form class="formulario max-w-sm mx-auto" @submit.prevent="adicionarProduto">
+            <h1 class="text-2xl my-3.5 text-center uppercase font-bold">Adicionar produto</h1>
+            <div class="mb-5">
+                <label class="block mb-2 text-sm font-medium text-gray-900" for="default_size">Escolha a imagem do
+                    produto</label>
+                <input
+                    class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
+                    id="default_size" ref="fileInput" type="file" @change="processarImagem">
+            </div>
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+                Adicionar Produto</button>
+        </form>
+
+        <!-- Card de Adicionar Vídeo -->
+        <form class="formulario max-w-sm mx-auto" @submit.prevent="adicionarVideo">
+            <div v-if="mensagemVideo"
+                :class="['mt-5 mb-5 border-l-4 p-4', tipoMensagemVideo === 'success' ? 'bg-green-100 border-green-500 text-green-700' : 'bg-red-100 border-red-500 text-red-700']"
+                role="alert">
+                <p class="font-bold" v-if="tipoMensagemVideo === 'error'">Erro</p>
+                <p class="font-bold" v-if="tipoMensagemVideo === 'success'">Sucesso</p>
+                <p>{{ mensagemVideo }}</p>
+            </div>
+            <h1 class="text-2xl my-3.5 text-center uppercase font-bold">Adicionar Vídeo</h1>
+            <div class="mb-5">
+                <label class="block mb-2 text-sm font-medium text-gray-900" for="videoProduto">Escolha o vídeo do
+                    produto</label>
+                <input
+                    class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
+                    id="videoProduto" ref="fileInputVideo" type="file" @change="processarVideo">
+            </div>
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+                Adicionar Vídeo</button>
+        </form>
+    </div>
+
+    <div class="bg-gray-100 mt-20">
+        <remover-videos ref="videos" />
+    </div>
+    <div class="bg-gray-200">
         <remover-produtos ref="produtos" />
     </div>
 </template>
@@ -35,10 +66,12 @@
 import { Options, Vue } from 'vue-class-component';
 import axios from 'axios';
 import RemoverProdutos from '../components/RemoverProdutos.vue'
+import RemoverVideos from '@/components/RemoverVideos.vue';
 
 @Options({
     components: {
-        RemoverProdutos
+        RemoverProdutos,
+        RemoverVideos
     }
 })
 
@@ -47,10 +80,18 @@ export default class AdicionarProduto extends Vue {
     mensagem: string | null = null  // Variável para armazenar a mensagem de erro
     tipoMensagem: 'success' | 'error' | null = null
 
+    mensagemVideo: string | null = null
+    tipoMensagemVideo: string | null = null
+
+    declare $refs: {
+        fileInput: HTMLInputElement
+        fileInputVideo: HTMLInputElement
+    }
+
     //logout
     public logout() {
-        localStorage.removeItem('user-authenticated');
-        this.$router.push('/login-admin');
+        localStorage.removeItem('user-authenticated')
+        this.$router.push('/login-admin')
     }
 
     //processar imagem
@@ -74,57 +115,122 @@ export default class AdicionarProduto extends Vue {
 
     // Após adicionar o produto no backend, adicione a imagem ao array de produtos diretamente no frontend:
     async adicionarProduto() {
-    if (!this.imagem) {
-        this.definirMensagem('Por favor, escolha uma imagem.', 'error');
-        return;
+
+        if (!this.imagem) {
+            this.definirMensagem('Por favor, escolha uma imagem.', 'error')
+            return
+        }
+
+        const formData = new FormData()
+        formData.append('imagem', this.imagem)
+
+        try {
+            const response = await axios.post(
+                'http://localhost/Projetos/ja-quero-pijamas/backend/api/adicionar_produto.php',
+                formData,
+                { headers: { 'Content-Type': 'multipart/form-data' } }
+            );
+
+            if (response.data.success) {
+                this.definirMensagem('Produto adicionado com sucesso!', 'success')
+
+                // Receber o ID do produto retornado pela API
+                const idProduto = response.data.id
+
+                // Emitir o evento com o produto, incluindo o id
+                this.$emit('produto-adicionado', { id: idProduto, imagem: this.imagem })
+
+                // Limpar o campo de input
+                const inputFile = this.$refs.fileInput as HTMLInputElement
+                if (inputFile) {
+                    inputFile.value = '' // Limpar o campo de input
+                    const label = inputFile.nextElementSibling as HTMLElement
+                    if (label) {
+                        label.innerText = 'Nenhum arquivo escolhido'
+                    }
+                }
+            } else {
+                this.definirMensagem(response.data.message || 'Erro ao adicionar o produto.', 'error')
+            }
+        } catch (error) {
+            console.error(error);
+            this.definirMensagem('Erro ao enviar o produto.', 'error')
+        }
     }
 
-    const formData = new FormData();
-    formData.append('imagem', this.imagem);
+    //adicionar video
+    async adicionarVideo() {
+        const formData = new FormData()
+        const fileInput = this.$refs.fileInputVideo as HTMLInputElement
 
-    try {
-        const response = await axios.post(
-            'http://localhost/Projetos/ja-quero-pijamas/backend/api/adicionar_produto.php',
-            formData,
-            { headers: { 'Content-Type': 'multipart/form-data' } }
-        );
+        if (fileInput && fileInput.files && fileInput.files[0]) {
+            const videoFile = fileInput.files[0]
+            formData.append("video", videoFile)
 
-        if (response.data.success) {
-            this.definirMensagem('Produto adicionado com sucesso!', 'success')
+            // Log do FormData
+            console.log("FormData sendo enviado:", formData)
 
-            // Receber o ID do produto retornado pela API
-            const idProduto = response.data.id
+            try {
+                const response = await axios.post(
+                    'http://localhost/Projetos/ja-quero-pijamas/backend/api/adicionar_video.php',
+                    formData,
+                    { headers: { 'Content-Type': 'multipart/form-data' } }
+                )
 
-            // Emitir o evento com o produto, incluindo o id
-            this.$emit('produto-adicionado', { id: idProduto, imagem: this.imagem })
+                console.log("Resposta do servidor:", response.data)
 
-            // Limpar o campo de input
-            const inputFile = this.$refs.fileInput as HTMLInputElement
-            if (inputFile) {
-                inputFile.value = '' // Limpar o campo de input
-                const label = inputFile.nextElementSibling as HTMLElement
-                if (label) {
-                    label.innerText = 'Nenhum arquivo escolhido'
+                if (response.data.success) {
+                    this.definirMensagem('Vídeo adicionado com sucesso!', 'success')
+                } else {
+                    this.definirMensagem('Erro ao adicionar vídeo.', 'error')
                 }
+
+                // Limpar o campo de input
+                const inputFile = this.$refs.fileInputVideo as HTMLInputElement
+                if (inputFile) {
+                    inputFile.value = '' // Limpar o campo de input
+                    const label = inputFile.nextElementSibling as HTMLElement
+                    if (label) {
+                        label.innerText = 'Nenhum arquivo escolhido'
+                    }
+                }
+
+            } catch (error) {
+                console.error('Erro de requisição:', error)
+                this.definirMensagem('Erro ao adicionar vídeo.', 'error')
             }
         } else {
-            this.definirMensagem(response.data.message || 'Erro ao adicionar o produto.', 'error')
+            this.definirMensagem('Por favor, selecione um vídeo primeiro.', 'error')
         }
-    } catch (error) {
-        console.error(error);
-        this.definirMensagem('Erro ao enviar o produto.', 'error');
     }
-}
 
+    //processar video
+    processarVideo(event: Event) {
+        const input = event.target as HTMLInputElement
+        const file = input.files ? input.files[0] : null
+
+        if (file) {
+            const allowedExtensions = ['mp4', 'avi', 'mkv', 'mov', 'webm']
+            const fileExtension = file.name.split('.').pop()?.toLowerCase()
+
+            if (fileExtension && allowedExtensions.includes(fileExtension)) {
+                return
+            } else {
+                console.error('Arquivo inválido. Apenas vídeos são permitidos.')
+                this.definirMensagem('Arquivo inválido. Apenas vídeos nos formatos mp4, avi, mkv, mov e webm são permitidos.', 'error')
+                this.$refs.fileInputVideo.value = ''
+            }
+        }
+    }
 
     definirMensagem(mensagem: string, tipo: 'success' | 'error') {
         this.mensagem = mensagem;
         this.tipoMensagem = tipo;
 
         setTimeout(() => {
-            this.mensagem = null;
-            this.tipoMensagem = null;
-        }, 5000);
+            this.mensagem = null
+            this.tipoMensagem = null
+        }, 5000)
     }
 }
 </script>
